208472e8fe945f1be189708d78cdebbcf6ff72ea
Victor Cionca <victorcionca@gmail.com>
2018-03-07 12:35:32 +0000

    Added the config_net_stack method to the node template

--- loranode_template.py
+++ loranode_template.py
@@ -15,6 +15,13 @@ class LoraNodeTemplate():
         self.config = config
         self.netstack = netstack
 
+    def config_net_stack(self, layers, logger, channel):
+        """
+        Creates a network stack for the node and binds it to
+        the physical layer and the channel.
+        """
+        pass
+
     def app_process(self):
         """
         Application process that generates packets
